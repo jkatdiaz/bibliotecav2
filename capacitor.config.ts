ffaba3db935593472470b0c0e4f20585b0e9366b
German Betancourt <germanbeta@gmail.com>
2024-10-29 22:19:04 -0500

    Refactor book editing and viewing components for improved UX

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,10 @@ const config: CapacitorConfig = {
   appId: "com.example.app",
   appName: "Biblioteca",
   webDir: "dist",
+  // server: {
+  //   // Permitir navegación a todas las subrutas bajo deuscaritasestf.org
+  //   allowNavigation: ["deuscaritasestf.org", "*.deuscaritasestf.org"],
+  // },
 };
 
 export default config;
--- src/components/VisualizarLibro.tsx
+++ src/components/VisualizarLibro.tsx
@@ -1,62 +1,77 @@
-// src/PdfViewer.tsx
-import React , { useEffect, useState } from 'react';
-import { IonContent, IonHeader, IonMenuButton, IonButtons, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/react';
-import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAnglesLeft } from '@fortawesome/free-solid-svg-icons';
-import Drawer from './Drawer';
-import { useLocation } from 'react-router-dom';
+import React, { useState } from "react";
+import {
+  IonContent,
+  IonHeader,
+  IonMenuButton,
+  IonButtons,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonButton,
+} from "@ionic/react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAnglesLeft } from "@fortawesome/free-solid-svg-icons";
+import Drawer from "./Drawer";
+import { Document, Page, pdfjs } from "react-pdf";
 
+// Configura el worker de PDF.js
+// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 const PdfViewer: React.FC = () => {
-    const pdfUrl = localStorage.getItem('pdfUrl');
+  const pdfUrl = localStorage.getItem("pdfUrl");
+  const [numPages, setNumPages] = useState<number | null>(null);
+  const [pageNumber, setPageNumber] = useState(1);
 
-    const resetVariable =()=>{
-        
-        localStorage.removeItem('pdfUrl')
-    }
-    return (
-        <>
-            <Drawer />
-            <IonPage className="custom-background" id="main-content">
-                <IonHeader>
-                    <IonToolbar>
-                        <IonButtons slot="start">
-                            <IonMenuButton></IonMenuButton>
-                        </IonButtons>
-                        <IonTitle className='text-font'>Biblioteca Digital</IonTitle>
-                    </IonToolbar>
-                </IonHeader>
-                <Link to="/libros" className="no-underline">
-                    <IonButton
-                        className="boton-volver"
-                        shape="round"
-                        color="medium"
-                        onClick={resetVariable}
-                       
+  const resetVariable = () => {
+    localStorage.removeItem("pdfUrl");
+  };
 
-                    >
-                        <FontAwesomeIcon style={{ padding: '4px' }} icon={faAnglesLeft} />
-                        <div className="text-font" style={{ textTransform: 'capitalize' }}>
-                            Atrás
-                        </div>
-                    </IonButton>
-                </Link>
-                <IonContent>
-                    {pdfUrl ? (
-                        <iframe
-                            src={pdfUrl}
-                            style={{ width: '100%', height: '80vh', border: 'none' }}
-                            title="PDF Viewer"
-                        />
-                    ) : (
-                        <div>No se encontró el PDF.</div>
-                    )}
-                 
-                </IonContent>
-            </IonPage>
-        </>
-    );
+  const onDocumentLoadSuccess = ({ numPages }: { numPages: number }) => {
+    setNumPages(numPages);
+  };
+
+  return (
+    <>
+      <Drawer />
+      <IonPage className="custom-background" id="main-content">
+        <IonHeader>
+          <IonToolbar>
+            <IonButtons slot="start">
+              <IonMenuButton />
+            </IonButtons>
+            <IonTitle className="text-font">Biblioteca Digital</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <Link to="/libros" className="no-underline">
+          <IonButton
+            className="boton-volver"
+            shape="round"
+            color="medium"
+            onClick={resetVariable}
+          >
+            <FontAwesomeIcon style={{ padding: "4px" }} icon={faAnglesLeft} />
+            <div className="text-font" style={{ textTransform: "capitalize" }}>
+              Atrás
+            </div>
+          </IonButton>
+        </Link>
+        <IonContent>
+          {pdfUrl ? (
+            <Document file={pdfUrl} onLoadSuccess={onDocumentLoadSuccess}>
+              <Page pageNumber={pageNumber} width={window.innerWidth * 0.8} />
+            </Document>
+          ) : (
+            <div>No se encontró el PDF.</div>
+          )}
+          {/* <p>
+            Página {pageNumber} de {numPages}
+          </p> */}
+        </IonContent>
+      </IonPage>
+    </>
+  );
 };
 
 export default PdfViewer;
